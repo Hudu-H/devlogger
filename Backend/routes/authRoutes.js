f79464d3d0f805b4fdd681113bd257b96a1a911e
Hudu <92208883+Hudu-H@users.noreply.github.com>
2025-06-11 17:21:41 +0000

    update controllers (#3)

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,4 +1,4 @@
-import express, { Router } from 'express';
+import express from 'express';
 
 // internal imports
 import { registerUser, loginUser } from '../controllers/authController.js';
--- Backend/routes/logRoutes.js
+++ Backend/routes/logRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
 // internal imports
-import { createLog, updateLog, getLog, deletelog } from '../controllers/logController.js';
+import { createLog, updateLog, getLog, deleteLog } from '../controllers/logController.js';
 
 const router = express.Router();
 
 router.route('/').get(getLog).post(createLog);
-router.route('/:id').put(updateLog).delete(deletelog);
+router.route('/:id').put(updateLog).delete(deleteLog);
 
 export default router;
