46b92963491ecd47f26fddde19bd7a7c5fc07641
Hudu <92208883+Hudu-H@users.noreply.github.com>
2025-06-14 11:50:37 +0000

    add tailwind (#10)

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 // internal imports
 import App from './App.jsx';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- Frontend/src/pages/dashboard.jsx
+++ Frontend/src/pages/dashboard.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
 function Dashboard() {
-	return <div>Dashboard</div>;
+	return <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind + Vite + React!</h1>;
 }
 
 export default Dashboard;
